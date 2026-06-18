915ced903ab21c800cf7f0862e070fc40ddcec95
sang <sangfs.it@gmail.com>
2026-06-18 12:55:55 +0700

    fix all

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 
 const Footer = () => {
@@ -15,7 +17,7 @@ const Footer = () => {
           Find us on
         </span>
         <div className="flex gap-2">
-          
+          <a
             href="https://www.facebook.com"
             target="_blank"
             rel="noopener noreferrer"
@@ -26,7 +28,7 @@ const Footer = () => {
               <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
             </svg>
           </a>
-          
+          <a
             href="https://www.instagram.com"
             target="_blank"
             rel="noopener noreferrer"
